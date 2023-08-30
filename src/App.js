839377b24e0a0fc0a66aa1d5c7328ee4fbef7f6d
Siddhant Dhanaji Kadam <siddh4194@gmail.com>
2023-08-30 20:31:06 +0530

    trial for the mern stack

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [result,setResult] = useState('');
+  function fetchInfo() {
+    fetch("http://localhost:4000/api/test",{
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'},
+    })
+    .then(response => response.json())
+    .then(data => setResult(data))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={fetchInfo}>
+         fetch
+      </button>
+      <div>result:{result}</div>
     </div>
   );
 }
